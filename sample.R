4084a14d8eabb78711b0ec4eaeb1d093d4c75e49
Jonathan Levy <jl@jlstiles.com>
2019-06-11 10:28:13 -0700

    fixing naming of cols

--- sample.R
+++ sample.R
@@ -63,11 +63,11 @@ summary_maker = function(data, cols, fcn) {
 
 # specify cols and fcn for L and A summaries for each time point
 L_summary_info = list(list(data = df_wide, cols = 2:3, fcn = f_L0),
-                      list(data = df_wide, cols = 5:6, fcn = f_L0),
-                      list(data = df_wide, cols = 9:10, fcn = f_L0))
+                      list(data = df_wide, cols = 6:7, fcn = f_L0),
+                      list(data = df_wide, cols = 10:11, fcn = f_L0))
 A_summary_info = list(list(data = df_wide, cols = 4:5, fcn = f_L0),
-                      list(data = df_wide, cols = 7:8, fcn = f_L0),
-                      list(data = df_wide, cols = 11:12, fcn = f_L0))
+                      list(data = df_wide, cols = 8:9, fcn = f_L0),
+                      list(data = df_wide, cols = 12:13, fcn = f_L0))
 
 # compute the summary measures for each time point
 L0_summaries = lapply(L_summary_info, FUN = function(L) {
@@ -83,10 +83,11 @@ A0_summaries = lapply(A_summary_info, FUN = function(L) {
 head(A0_summaries[[1]])
 
 # compile df for each time point, including summaries is right order with names
-dfs_per_time = lapply(1:3, FUN = function(t) {
-  df_new = cbind(df_wide[,L_summary_info[[t]]$cols], L0_summaries[[t]], 
-                 df_wide[,A_summary_info[[t]]$cols], A0_summaries[[t]])
-  colnames(df_new)[c(3:4, 7:8)] = c(paste0(c("L0_sum1_", "L0_sum2_"), t), paste0(c("A0_sum1_", "A0_sum2_"), t))
+dfs_per_time = lapply(0:2, FUN = function(t) {
+  df_new = cbind(df_wide[,L_summary_info[[t+1]]$cols], L0_summaries[[t+1]], 
+                 df_wide[,A_summary_info[[t+1]]$cols], A0_summaries[[t+1]])
+  colnames(df_new)[c(3:4, 7:8)] = c(paste0("L",t,"_sum1"), paste0("L",t,"_sum2"),
+                                    paste0("A",t,"_sum1"), paste0("A",t,"_sum2"))
   return(df_new)
   
 })
@@ -99,5 +100,6 @@ head(df_new)
 
 # Can now use sl3, where we can run a superlearner and predict, 
 # then use as outcomes for the next regression.  That is, just use sl3 
-# but specify the ID variable.  
+# but specify the ID variable.  To predict under intervention, just make
+# new task with intervened-upon treatments and predict
 
